aaa94eafff5ac00275001a6f834e41893cc48e42
ali <alisojod20@gmail.com>
2026-04-15 15:46:38 +0200

    change

--- Main.java
+++ Main.java
@@ -3,5 +3,9 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("hello värld");
+        System.out.println("hej");
+        System.out.println("ddd");
+        System.out.println("sss");
     }
+
 }
