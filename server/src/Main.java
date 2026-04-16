c7d2ab4de9a61f20dbab7d0695136bd2c02c21d6
ali <alisojod20@gmail.com>
2026-04-16 13:03:39 +0200

    setup Kommunikation

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,9 +1,27 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLOutput;
+
 public class Main
 {
-    public static void main(String[] args)
-    {
-        System.out.println("hello värld");
+    public static void main(String[] args) throws IOException {
+        //port som kommunikation sker på
+        int port = 8000;
+        //socket är end-point för kommunikation mellan två enheter.
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("System körs på port: " + port);
+        while (true){
+            Socket clientSocket = serverSocket.accept();
+            InputStream inputStream = clientSocket.getInputStream();
+            OutputStream outputStream = clientSocket.getOutputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader in = new BufferedReader(inputStreamReader);
+            PrintWriter out = new PrintWriter(outputStream, true);
 
-        System.out.println("JAG skriva");
+            //incomingData är data som skickas från både IS/klient till socket
+            String incomingData = in.readLine();
+
+        }
     }
 }
