4f7da97a48a4155b652b8427e7dbb5d9f669844f
ali <alisojod20@gmail.com>
2026-04-17 15:06:31 +0200

    test

--- server/src/Main.java
+++ server/src/Main.java
@@ -4,5 +4,7 @@ public class Main
 {
     public static void main(String[] args){
         Controller controller = new Controller();
+        System.out.println("test" +
+                "");
     }
 }
