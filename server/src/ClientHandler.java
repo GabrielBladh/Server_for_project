90750365bd1514efc3f1ce991fa2a4f2d182efa1
ali <alisojod20@gmail.com>
2026-05-29 13:40:58 +0200

    update

--- server/src/ClientHandler.java
+++ server/src/ClientHandler.java
@@ -110,7 +110,6 @@ public class ClientHandler implements Runnable {
                         continue;
                     }
                     String response = controller.getGame().getGameEnd();
-                    System.out.println(response);
                     out.println(response);
                     out.flush();
 
